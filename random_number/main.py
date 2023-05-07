83acaf55183b51e64812ec077050f899b12480a6
Toby <kommandertoby@gmail.com>
2023-05-07 06:45:18 +0100

    Improve Comments

--- random_number/main.py
+++ random_number/main.py
@@ -1,8 +1,10 @@
 # This program "spins the wheel" and keeps spinning until it gets a different number
 from random import randint
 
+# make x a number between 1 and 23
 x = randint(1, 23)
+# make y equal to x
 y = x
-while x == y:
-    print(y)
-    y = randint(1, 23)
+while x == y: # while x and y are the same
+    print(y) # print y
+    y = randint(1, 23) # make y a new random number (if it is the same as before, it will do this again)
